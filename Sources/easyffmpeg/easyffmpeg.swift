1f9f96a3c0d2f4b139ea7497427c6ba3e4e8d5cf
Haider Awan <haid3rawan@icloud.com>
2025-03-29 19:13:55 +0500

    call back and init function

--- Sources/easyffmpeg/easyffmpeg.swift
+++ Sources/easyffmpeg/easyffmpeg.swift
@@ -1,25 +1,14 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
 import ffmpegkit
 import SwiftUI
 
 public class FFmpegViewModel: ObservableObject {
     
-    public init() {
-        // Enable FFmpeg log callback
-        FFmpegKitConfig.enableLogCallback { log in
-            if let logMessage = log?.getMessage() {
-                print("📜 FFmpeg Log: \(logMessage)")
-            }
-        }
-    }
+    public init() {}
 
     public func createRemoteFileList(videoURLs: [String]) -> URL? {
         let fileListContent = videoURLs.map { "file '\($0)'" }.joined(separator: "\n")
-        
         let fileListURL = FileManager.default.temporaryDirectory.appendingPathComponent("file_list.txt")
-        
+
         do {
             try fileListContent.write(to: fileListURL, atomically: true, encoding: .utf8)
             return fileListURL
@@ -34,28 +23,24 @@ public class FFmpegViewModel: ObservableObject {
             completion(nil)
             return
         }
-        
-        let outputURL = FileManager.default.temporaryDirectory.appendingPathComponent("\(UUID().uuidString).mp4")
-        var command: String
 
-        if videoURLs.count == 1, let firstVideoUrl = videoURLs.first {
-            command = """
-            -i "\(firstVideoUrl)" -c copy "\(outputURL.path)"
-            """
-        } else {
-            command = """
-            -protocol_whitelist file,http,https,tcp,tls -f concat -safe 0 -i "\(fileList.path)" -c copy "\(outputURL.path)"
-            """
+        let outputURL = FileManager.default.temporaryDirectory.appendingPathComponent("\(UUID()).mp4")
+        let command = """
+        -protocol_whitelist file,http,https,tcp,tls -f concat -safe 0 -i \(fileList.path) -c copy \(outputURL.path)
+        """
+
+        FFmpegKitConfig.enableLogCallback { log in
+            if let logMessage = log?.getMessage() {
+                print("📜 FFmpeg Log: \(logMessage)")
+            }
         }
-        
-        print("🔹 Processing \(videoURLs.count) video(s) with FFmpeg.")
-        
+
         FFmpegKit.executeAsync(command) { session in
             if let returnCode = session?.getReturnCode(), returnCode.isValueSuccess() {
-                print("✅ Download & Merge Successful: \(outputURL.path)")
+                print("✅ Download & Merge Successful: \(outputURL)")
                 completion(outputURL)
             } else {
-                print("❌ FFmpeg Error: \(session?.getFailStackTrace() ?? "Unknown error")")
+                print("❌ Error: \(session?.getFailStackTrace() ?? "Unknown error")")
                 completion(nil)
             }
         }
